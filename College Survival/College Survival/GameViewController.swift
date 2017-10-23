7e60ca8559912d5357ae4559f48fc130b8c9ca53
Sarah Mai <sarahmai434@gmail.com>
2017-10-23 16:25:03 -0700

    start changes to code to GameScene and GameViewController

--- College Survival/College Survival/GameViewController.swift
+++ College Survival/College Survival/GameViewController.swift
@@ -17,22 +17,11 @@ class GameViewController: UIViewController
     {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView?
-        {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene")
-            {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        
+        let skView = self.view as! SKView
+        let scene = GameScene()
+        scene.scaleMode = .aspectFit
+        skView.presentScene(scene)
     }
 
     override var shouldAutorotate: Bool
