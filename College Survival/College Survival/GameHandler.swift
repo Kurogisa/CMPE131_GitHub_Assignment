4d7984b9766511827a6bbca568946f3391a9d1d4
Sarah Mai <sarahmai434@gmail.com>
2017-11-06 17:49:47 -0800

    more coding on new Class and added Level01.plist

--- College Survival/College Survival/GameHandler.swift
+++ College Survival/College Survival/GameHandler.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+
+class GameHandler
+{
+    var score:Int
+    var highScore:Int
+    var flowers:Int
+    
+    var levelData:NSDictionary!
+    
+    class var sharedInstances:GameHandler
+    {
+        struct Singleton
+        {
+            static let instance = GameHandler()
+        }
+        return Singleton.instance
+    }
+    
+    init()
+    {
+        score = 0
+        highScore = 0
+        flowers = 0
+        
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        
+        highScore = userDefaults.integerForKey("highScore")
+        flowers = userDefaults.integerForKey("flowers")
+        
+        if let path = NSbundle.mainBundle().pathForResources("Level01") //1:04:38
+        
+    }
+    
+    func saveGameStats()
+    {
+        highScore = max(score,highScore)
+        
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        userDefaults.setInteger(highScore, forKey: "highScore")
+        userDefaults.setInteger(flowers, forKey: "flowers")
+        userDefaults.synchronize()
+    }
+}
